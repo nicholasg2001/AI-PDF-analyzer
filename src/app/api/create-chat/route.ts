7284c6b375e05722ec61fb6bed886105f37c8bf2
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-05-09 13:37:18 -0400

    update POST route

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,15 +1,38 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 import { loadS3IntoPineCone } from "@/lib/pinecone";
+import { getS3Url } from "@/lib/s3";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
 // /api/create-chat
 export async function POST(req: Request, res: Response){
+
+    const { userId } = await auth()
+    if(!userId){
+        return NextResponse.json({ error: "unauthorized" }, { status: 401 })
+    }
+
     try {
         const body = await req.json()
         const {file_key, file_name} = body
         console.log(file_key, file_name);
-        const pages = await loadS3IntoPineCone(file_key);
-        return NextResponse.json({ pages })
+        await loadS3IntoPineCone(file_key);
+        const chat_id = await db.insert(chats).values({
+            fileKey: file_key,
+            pdfName: file_name,
+            pdfUrl: getS3Url(file_key),
+            userId: userId
+        }).returning({
+            insertedId: chats.id,
+        });
+
+
+        return NextResponse.json({
+            chat_id: chat_id[0].insertedId
+        }, { status: 200 })
+        
     } catch (error) {
         console.error(error);
         return NextResponse.json(
