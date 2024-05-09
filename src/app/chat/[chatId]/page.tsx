1d3da03c473d9ac28f028d843583aef612cc5d1f
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-05-09 13:59:52 -0400

    Create scaffolding for chats page

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -37,7 +37,22 @@ const ChatPage = async ({ params : { chatId } }: Props) => {
 
     return (
         <div className="flex max-h-screen overflow-scroll">
-            
+            <div className="flex w-full max-h-screen overflow-scroll">
+                {/* chat sidebar */}
+                <div className="flex-[1] max-w-xs">
+                    <ChatSideBar />
+                </div>
+
+                {/* pdf viewer */}
+                <div className="max-h-screen p-4 overflow-scroll flex-[5]">
+                    <PDFViewer />
+                </div>
+
+                {/* chat component */}
+                <div className="flex-[3] border-l-4 border-l-slate-200 dark:border-l-slate-700">
+                    <ChatComponent />
+                </div>
+            </div>
         </div>
     )
 }
